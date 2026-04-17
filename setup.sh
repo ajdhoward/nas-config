f5a0143eaee88ea59fcd85b59ff5e6d9b8607baf
Adam Howard <adam@pop-os>
2026-04-17 13:39:50 +0100

    Initial NAS setup script

---
+++ setup.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+# --- 1. MOUNT STORAGE ---
+mkdir -p /mnt/media
+mount /dev/sdb1 /mnt/media 
+
+# --- 2. CONFIGURE WAKE-ON-LAN ---
+echo -e "auto eth0\niface eth0 inet dhcp\n    ethernet-wol g" > /etc/network/interfaces.d/eth0
+apt-get update && apt-get install -y ethtool
+ethtool -s eth0 wol g
+
+# --- 3. INSTALL SOFTWARE ---
+dietpi-software install 130 137 134 135 146 162
+
+# --- 4. SETUP AIOSTREAMS & METADATA (YOUR FORKS) ---
+git clone https://github.com /home/dietpi/AIOStreams
+cd /home/dietpi/AIOStreams && npm ci --omit=dev
+
+git clone https://github.com /home/dietpi/AIOMetadata
+cd /home/dietpi/AIOMetadata && npm ci --omit=dev
+
+# --- 5. PROCESS MANAGEMENT ---
+npm install -g pm2
+pm2 start /home/dietpi/AIOStreams/index.js --name "aiostreams"
+pm2 start /home/dietpi/AIOMetadata/index.js --name "aiometadata"
+pm2 save
+pm2 startup
